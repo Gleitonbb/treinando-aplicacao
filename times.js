41e1a7407756159d38a02aba7b677fff94756294
Gleiton Brito Bernardes <154294770+Gleitonbb@users.noreply.github.com>
2024-01-26 09:31:15 -0300

    concluido

--- times.js
+++ times.js
@@ -1,23 +1,22 @@
-const caixa1 = document.getElementById('caixa1')
-const caixa2 = document.querySelector('#caixa2')
-const botao = document.querySelector('#btn_copiar')
 const times = [...document.querySelectorAll('.time')]
+const caixa1 = document.querySelector('#caixa1')
+const caixa2 = document.querySelector('#caixa2')
+const botao = document.querySelector('#botao')
 
 times.map((upu)=>{
     upu.addEventListener('click',(evt)=>{
-     const cli = evt.target
-     cli.classList.toggle('selecionado')
+        const cadaUm = evt.target
+        cadaUm.classList.toggle("selecionado")
     })
-    botao.addEventListener('click',()=>{
-        let tdTime = [...document.querySelectorAll('.selecionado')]
-        let tdTime2 = [...document.querySelectorAll('.time:not(.selecionado)')]
-        tdTime.map((tpt)=>{
-        caixa2.appendChild(tpt)
-
-        })
-        tdTime2.map((tpt)=>{
-            caixa1.appendChild(tpt)
-        })
-        
+     })
+       botao.addEventListener('click',()=>{
+     const tranEleme = [...document.querySelectorAll('.selecionado')]
+     const traele = [...document.querySelectorAll('.time:not(.selecionado)')]
+     tranEleme.map((upu)=>{
+        caixa2.appendChild(upu)
     })
+    traele.map((upu)=>{
+        caixa1.appendChild(upu)
+    })
+   
 })
